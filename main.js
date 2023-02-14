00bcffa5fcc91bd6de728c5e30afa3403068b5c3
Anastasiia4435 <107216310+Anastasiia4435@users.noreply.github.com>
2023-02-14 18:21:00 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+let a = 20;
+
+if (a > 10) {
+    alert ("OK");
+}
+else {
+    alert (false);
+}
+
+let b = "Andrew";
+
+if (b == "Andrew") {
+    alert("Hello");
+}
+else {
+    alert("Who are you?");
+}
